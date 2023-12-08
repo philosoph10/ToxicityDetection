8d14753dac8ac889a3031959d66b0ac3391bd9fe
dieumerci7 <yarema.dekhtiar@gmail.com>
2023-12-08 12:25:12 +0200

    Change evaluate.py, test_evaluate.py

--- unit_test/test_evaluate.py
+++ unit_test/test_evaluate.py
@@ -1,10 +1,5 @@
 import numpy as np
-# from sklearn.datasets import load_iris
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.multioutput import MultiOutputClassifier
-
-from utils.evaluate import mean_roc_auc  # , cross_validate
-# from utils.model import ToxicityDetector
+from utils.evaluate import mean_roc_auc
 
 
 def test_mean_roc_auc():
@@ -14,18 +9,3 @@ def test_mean_roc_auc():
 
     assert isinstance(score, np.float64)
     assert 0 <= score <= 1
-
-
-# def test_cross_validate():
-#     X = ["Hello! My name is Yarema.", "How are you?", "This sentence is very-very toxic."]
-#     y = np.array([[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [1, 1, 0, 0, 0, 0]]).reshape(3, 6)
-#     classifier = ToxicityDetector()
-#     # X = np.array([1, 2, 3, 4, 5, 6]).reshape(6, 1)
-#     # y = np.array([[True, True], [True, False], [False, False], [False, True],
-#     #               [True, True], [False, True]]).reshape(6, 2)
-#     # X, y = load_iris(return_X_y=True)
-#     # classifier = MultiOutputClassifier(LogisticRegression())
-#     score = cross_validate(classifier, X, y)
-#
-#     assert isinstance(score, np.float64)
-#     assert 0 <= score <= 1
--- utils/evaluate.py
+++ utils/evaluate.py
@@ -1,6 +1,5 @@
 import numpy as np
-from sklearn.metrics import roc_auc_score, make_scorer
-from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_auc_score
 
 
 def mean_roc_auc(y_true, y_pred_proba):
@@ -20,17 +19,3 @@ def mean_roc_auc(y_true, y_pred_proba):
         roc_auc = roc_auc_score(y_true[:, i], y_pred_proba[:, i])
         roc_auc_scores.append(roc_auc)
     return np.mean(roc_auc_scores)
-
-
-def cross_validate(classifier, X, y, cv=3):
-    """
-    Perform Cross-Validation to test the model's performance
-    :param classifier: the classification model
-    :param X: texts for cross-validation
-    :param y: labels for cross-validation
-    :param cv: cross-validation strategy; default=3, meaning 3-fold cross-validation
-    :return: the mean cross-validation score
-    """
-    custom_scorer = make_scorer(mean_roc_auc, greater_is_better=True, needs_proba=True)
-
-    return np.mean(cross_val_score(classifier, X, y, cv=cv, scoring=custom_scorer))
